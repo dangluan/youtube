68b0edf5b814b919748f84bb57289e9e72cabe82
Peter Dang <peter@rubify.com>
2013-05-10 11:45:09 +0700

    fix

--- youtube/config/environments/development.rb
+++ youtube/config/environments/development.rb
@@ -34,5 +34,5 @@ Youtube::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
-  Paperclip.options[:command_path] = "/usr/bin/"
+  Paperclip.options[:command_path] = "/usr/local/bin/"
 end
