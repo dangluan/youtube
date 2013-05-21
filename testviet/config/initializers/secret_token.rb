2d18ee26ac6bd5d436589810ef27243bebb795da
Rubify <peter@rubify.com>
2013-05-21 17:23:32 +0700

    first commit

---
+++ testviet/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Testviet::Application.config.secret_token = 'b87230c752249bca6ed86c71047dc0f058ef54970352d7a86ab042dcb5de3069591258249fcbe00552be7dc21eecab72568288a1c7c6df7657d0d86a7824bacb'
